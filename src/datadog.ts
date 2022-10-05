728e5637b32e1d3f7126f4d88c29479536b8ccca
Pawel Laskarzewski <73839878+PawelLampliapps@users.noreply.github.com>
2022-10-05 11:22:23 +0200

    chore: DD code clean up

--- src/datadog.ts
+++ src/datadog.ts
@@ -12,8 +12,8 @@ export const config = new DdSdkReactNativeConfiguration(
   DDClientToken,
   __DEV__ ? 'development' : 'production',
   applicationID,
-  false, // track User interactions (disabled because most user interactions are tracked via custom button and pressable components)
-  false, // track XHR Resources (disabled because all requests are manually tracked via interceptors)
+  false, // track User interactions (not required)
+  false, // track XHR Resources (not required)
   !__DEV__, // track Errors
 );
 config.site = 'US1';
@@ -21,8 +21,6 @@ config.verbosity = SdkVerbosity.WARN;
 config.serviceName = serviceName;
 // Optional: enable or disable native crash reports
 config.nativeCrashReportEnabled = true;
-
-config.additionalConfig;
 
 export const initializeDatadog = async (): Promise<void> => {
   await DdSdkReactNative.initialize(config);
